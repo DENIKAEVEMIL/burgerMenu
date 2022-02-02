f93bc356a816f7929a6fa7e75d22cc8eb050d894
Denikaev Emil <edenikaev@mail.ru>
2022-02-03 01:54:16 +0400

    Готовое бургер меню

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ if (isMobile.any()){
   let menuArrows = document.querySelectorAll('.menu__arrow');
   if (menuArrows.length > 0) {
     menuArrows.forEach(arrow => arrow.addEventListener("click", function(e){
-      arrow.parentElement.classList.toggle('_active')
+      arrow.parentElement.classList.toggle('_active');
     })
     )
   }
@@ -40,18 +40,34 @@ if (isMobile.any()){
   document.body.classList.add('_pc');
 }
 
-// Прокрутка при клике
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu){
+  iconMenu.addEventListener("click", function(e){
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  })
+}
 
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0){
   menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", onMenuLinkClick)
+    menuLink.addEventListener("click", onMenuLinkClick);
   });
   function onMenuLinkClick(e){
     const menuLink = e.target;
     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+      if (iconMenu.classList.contains('_active')){
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
 
       window.scrollTo({
         top: gotoBlockValue,
